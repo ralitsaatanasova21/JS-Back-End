8d00a962014ed79522e3b7748801101a31abb4a9
Ralitsa Atanasova <ralitsa.atanasova21@gmail.com>
2022-06-08 22:33:17 +0300

    Implemented car listing, added front-end code

---
+++ Carbicle/index.js
@@ -0,0 +1,56 @@
+// [x] initialize and configure Express app
+// [x] initialize templating lib
+// [x] create home controller
+// [x] bind routing
+// [x] create layout
+// create data service
+// [x] read all
+// [x] read one by id
+// [x] create
+// [] edit
+// [] delete
+// [x] search
+// implement controllers
+// [x] home
+// [x] details
+// [x] create
+// [x] about
+// [x] add front-end code
+
+
+
+
+const express=require('express');
+const hbs=require('express-handlebars');
+const carService=require('./services/cars');
+
+const { about } = require('./controllers/about');
+const  create  = require('./controllers/create');
+const { details } = require('./controllers/details');
+const { home } = require('./controllers/home');
+const { notFound } = require('./controllers/notFound');
+
+const app=express();
+
+app.engine('hbs', hbs.create({
+    extname: '.hbs'
+}).engine);
+
+app.set('view engine', 'hbs');
+
+
+app.use(express.urlencoded({extended: true}));
+app.use("/static", express.static('static'));
+app.use(carService())
+
+app.get('/', home);
+app.get('/about', about);
+app.get('/details/:id', details);
+app.route('/create').get(create.get).post(create.post);
+
+
+app.all('*', notFound);
+
+
+
+app.listen(3000, ()=>console.log('ready'));
---
+++ Carbicle/static/js/app.js
@@ -0,0 +1,12 @@
+document.getElementById("cars").addEventListener("click", (e) => {
+  if (e.target.classList.contains("more")) {
+    const desc = e.target.parentElement.querySelector(".description");
+    if (desc.style.display == "block") {
+      desc.style.display = "none";
+      e.target.textContent = "Show More";
+    } else {
+      desc.style.display = "block";
+      e.target.textContent = "Hide";
+    }
+  }
+});
